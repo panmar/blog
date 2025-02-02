822a5c518e8dd2502a4753fc296345ce327143d0
Marcin Panasiuk <panasiuk.marcin@gmail.com>
2025-02-02 11:27:15 +0100

    Update output dir and github branch

--- publishconf.py
+++ publishconf.py
@@ -16,6 +16,8 @@ CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
 
+OUTPUT_PATH = "docs"
+
 # Following items are often useful when publishing
 
 # DISQUS_SITENAME = ""
